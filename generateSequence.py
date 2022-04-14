72b4bf7982725319cc43dd4bb50e91c9e63c8f98
sbarnett <sfhbarnett91@gmail.com>
2022-04-14 15:52:06 +0200

    Add script to strip sequence from PDB file and convert to single letter code

---
+++ generateSequence.py
@@ -0,0 +1,17 @@
+
+path = '/Users/sbarnett/Documents/MATLAB/lineariseProtein/humantalin1-AF-Q9Y490-F1-model_v2.pdb'
+
+sequence = []
+
+aminos = {'MET':'M','VAL':'V','ALA':'A','LEU':'L','SER':'S','LYS':'K','ILE':'I','GLY':'G',
+          'ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','HIS':'H','PHE':'F','PRO':'P',
+          'THR':'T','TRP':'W','TYR':'Y','ARG':'R'}
+
+with open(path) as inputpdb:
+    data = inputpdb.readlines()
+    for line in data:
+        line = line.split()
+        if line[0] == 'SEQRES':
+            for entry in line[4:]:
+                sequence.append(aminos[entry])
+print("".join(s for s in sequence))
